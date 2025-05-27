a84686c5f7a5c6e11b8d916262a3354e5a0edb0f
shinsuke-mat <shinsuke-mat@users.noreply.github.com>
2025-05-27 17:43:56 +0900

    add initial resources

---
+++ app/src/main/java/osakau/swdesign/SemverValidator.java
@@ -0,0 +1,13 @@
+package osakau.swdesign;
+
+public class SemverValidator {
+	// https://gist.github.com/jhorsman/62eeea161a13b80e39f5249281e17c39
+	static final private String matcher =
+			"^([0-9]|[1-9][0-9]*)\\."
+			+ "([0-9]|[1-9][0-9]*)\\."
+			+ "([0-9]|[1-9][0-9]*)"
+			+ "(?:-([0-9A-Za-z-]+(?:\\.[0-9A-Za-z-]+)*))?(?:\\+[0-9A-Za-z-]+)?$";
+    public static boolean isValid(final String semver) {
+        return semver.matches(matcher);
+    }
+}
---
+++ app/src/test/java/osakau/swdesign/SemverValidatorTest.java
@@ -0,0 +1,37 @@
+package osakau.swdesign;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SemverValidatorTest {
+	@ParameterizedTest
+	@ValueSource(strings = {
+			"1.2.3",
+			"1.2.99",
+			"1.2.0",
+			"0.0.0",
+			"10.20.30",
+			"99999999.99999999.99999999",
+			})
+    void isValid(final String ver) {
+        assertTrue(SemverValidator.isValid(ver));
+    }
+
+	@ParameterizedTest
+	@ValueSource(strings = {
+			"1",
+			"1.2",
+			"1.2.",
+			"1.2.3.",
+			"1..3",
+			"aaa",
+			"1.01.1",
+			"1. 2.3",
+			"1.-2.3",
+			})
+    void isInvalid(final String ver) {
+        assertFalse(SemverValidator.isValid(ver));
+    }
+}
